1fde6f982ec5c24468f976a781c3e6244d532533
Liz Puhl <eapuhl@gmail.com>
2016-08-15 13:28:20 -0500

    basic functionality all works

--- server/app.js
+++ server/app.js
@@ -6,21 +6,25 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var solution = 0;
+var solution = {};
 
 app.post('/problemsolve', function (req, res) {
   var problem = req.body;
-  if (problem.type = "add") {
-    solution = problem.firstvalue += problem.secondvalue;
-  } else if (problem.type = "subtract") {
-    solution = problem.firstvalue - problem.secondvalue;
-  } else if (problem.type = "multiply") {
-    solution = problem.firstvalue * problem.secondvalue;
-  } else if (problem.type = "divide") {
-    solution = problem.firstvalue / problem.secondvalue;
+  problem.firstvalue = Number(problem.firstvalue);
+  problem.secondvalue = Number(problem.secondvalue);
+  console.log('Problem: ', problem);
+
+  if (problem.type == "add") {
+    solution.answer = problem.firstvalue += problem.secondvalue;
+  } else if (problem.type == "subtract") {
+    solution.answer = problem.firstvalue - problem.secondvalue;
+  } else if (problem.type == "multiply") {
+    solution.answer = problem.firstvalue * problem.secondvalue;
+  } else if (problem.type == "divide") {
+    solution.answer = problem.firstvalue / problem.secondvalue;
   }
   res.sendStatus(200);
-  console.log(solution);
+  console.log('Solution: ', solution.answer);
 });
 
 app.get('/problemsolve', function (req, res) {
@@ -33,7 +37,6 @@ app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   res.sendFile(path.join(__dirname, './public', file));
 });
-
 
 app.listen(app.get('port'), function() {
   console.log('server is running on port ', app.get('port'));
--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
     event.preventDefault();
     // create "type of operator" property on problem object
     problem.type = $(this).attr('id');
-    console.log('operator: ', problem.type);
+    // console.log('Operator: ', problem.type);
   });
 
-  $('.calc-app').on('submit', function(event) {
+  $('.numberinputs').on('submit', function(event) {
     event.preventDefault();
-    $.each($(".calc-app").serializeArray(), function(i, field) {
+    $.each($(".numberinputs").serializeArray(), function(i, field) {
       problem[field.name] = field.value;
     });
-    console.log('problem: ', problem);
+    console.log('Problem: ', problem);
 
     $.ajax({
       type: 'POST',
@@ -21,7 +21,7 @@ $(document).ready(function() {
       data: problem,
       success: function(data) {
         getAnswer();
-        console.log('post request successful!');
+        // console.log('post request successful!');
       },
       error: function() {
         console.log('post failed');
@@ -29,13 +29,20 @@ $(document).ready(function() {
       }
     });
 
+    $('#clear').click(function() {
+      $('#answer').empty();
+      $('form').find("input[type=text], textarea").val("");
+    });
+
     function getAnswer() {
       $.ajax({
         type: 'GET',
         url: '/problemsolve',
         success: function(solution) {
-          $("#answer").empty();
-          $('#answer').append(solution);
+          $('#answer').empty();
+          $('#answer').append(solution.answer);
+          // console.log (solution.answer);
+
         },
         error: function () {
           console.log('Calculator did not work');
